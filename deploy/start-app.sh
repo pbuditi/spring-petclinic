e254d526381a1e0fb455bfdb00e5541e1e025c6b
pbuditi <pbuditi@gmail.com>
2017-06-05 17:52:29 +0800

    Update start-app.sh

--- deploy/start-app.sh
+++ deploy/start-app.sh
@@ -1,3 +1,13 @@
-nohup java -jar app.jar > log.txt 2> errors.txt < /dev/null &
-PID=$!
-echo $PID > pid.txt
+#!/bin/sh
+SERVICE_NAME=petclinic
+PATH_TO_JAR=/home/cloud-user/petclinic/app.jar
+PID_PATH_NAME=/home/cloud-user/petclinic/pid.txt
+
+echo "Starting $SERVICE_NAME ..."
+  if [ ! -f $PID_PATH_NAME ]; then
+    nohup java -jar $PATH_TO_JAR /tmp 2>> /dev/null >> /dev/null &
+    echo $! > $PID_PATH_NAME
+    echo "$SERVICE_NAME started ..."
+ else
+    echo "$SERVICE_NAME is already running ..."
+ fi
