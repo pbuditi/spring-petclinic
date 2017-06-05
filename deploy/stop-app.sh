bd3641a6ca5c4e020aea3ad0d5fbfb5c6d83ea9e
pbuditi <pbuditi@gmail.com>
2017-06-05 17:49:56 +0800

    Update stop-app.sh

--- deploy/stop-app.sh
+++ deploy/stop-app.sh
@@ -1,2 +1,13 @@
-PID=$(cat pid.txt)
-kill $PID
+#!/bin/bash
+SERVICE_NAME=petclinic
+PID_PATH_NAME=/home/cloud-user/petclinic/pid.txt
+
+if [ -f $PID_PATH_NAME ]; then
+            PID=$(cat $PID_PATH_NAME);
+            echo "$SERVICE_NAME stoping ..."
+            kill $PID;
+            echo "$SERVICE_NAME stopped ..."
+            rm $PID_PATH_NAME
+        else
+            echo "$SERVICE_NAME is not running ..."
+        fi
